5aa481b1ad5aa52a976c39172606c5d03a22fdb7
inoue-ryoki <mth.1021@icloud.com>
2023-08-20 11:24:27 +0900

    alertが出るまで

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+window.App = consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,10 +10,10 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    alert(data['message']);
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function (message) {
+    return this.perform('speak', { message: message });
   }
 });

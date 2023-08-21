8422cbfdbec437f807a4a450ba4a1d5b03048868
inoue-ryoki <mth.1021@icloud.com>
2023-08-21 09:29:43 +0900

    チャットができるまで

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,7 +2,15 @@ import consumer from "./consumer"
 
 window.App = consumer.subscriptions.create("RoomChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
+    document.
+      querySelector('input[data-behavior="room_speaker"]').
+      addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          this.speak(event.target.value);
+          event.target.value = '';
+          return event.preventDefault();
+        }
+      });
   },
 
   disconnected() {
@@ -10,7 +18,8 @@ window.App = consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    alert(data['message']);
+    const element = document.querySelector('#messages')
+    element.insertAdjacentHTML('beforeend', data['message'])
   },
 
   speak: function (message) {
